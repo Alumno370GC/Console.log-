3a96db154c2cd80a3491e2662ff8c9c4ab126b7b
Alumno370GC <alumnoitleon@gmail.com>
2024-05-28 13:09:09 -0600

    first commit

---
+++ Console.js
@@ -0,0 +1,9 @@
+/**In JavaScript, the console.log () method displays messages or variables in the browser's console. */
+
+let message ="Hello, JavaScript";
+console.log(message);
+
+
+/*Example 1 */
+console.log("Good Morning");
+console.log(2000);
